e845237a92af1f43ec4919bded2898f1f4f89b9e
12moi <12moi>
2022-04-02 10:00:26 +0300

    update momodels

--- insta/models.py
+++ insta/models.py
@@ -1,7 +1,6 @@
 
 
 from distutils.command.upload import upload
-from email.mime import image
 from email.policy import default
 from unicodedata import name
 from django.db import models
@@ -10,12 +9,14 @@ from cloudinary.models import CloudinaryField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(max_length=500, default='My Bio', blank=True)
     name = models.CharField(blank=True, max_length=150)
     profile_pic = CloudinaryField('profile_pic')
+    location = models.CharField(max_length=60, blank=True)
 
     def __str__(self):
         return f'{self.user.username}'
@@ -43,16 +44,32 @@ class Image(models.Model):
     image = CloudinaryField('image')
     name = models.CharField(max_length=150, blank=True)
     caption = models.CharField('Caption(optional)', max_length=400, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
     upload_date = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField(default=0)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
 
     class Meta:
-        ordering = ['-upload_date']
+        ordering = ["-pk"]
 
-    
+    def get_absolute_url(self):
+        return f"/post/{self.id}"
+
+    @property
+    def get_all_comments(self):
+        return self.comments.all()
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def total_likes(self):
+        return self.likes.count()
+
     def __str__(self):
-        return f'{self.user.username} Image'
+        return f'{self.user.name} Post'
+
 
 class Follow(models.Model):
     followers = models.ForeignKey(Profile,on_delete=models.CASCADE, related_name='following')
@@ -62,10 +79,10 @@ class Follow(models.Model):
         return f'{self.followers} Follow'
 
 class Comments(models.Model):
-    comment = models.TextField(max_length=400)
-    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE,related_name='comments')
+    comment = models.TextField()
+    # post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
-    comment_date = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
 
     class Meta:
         ordering = ["-comment_date"]
