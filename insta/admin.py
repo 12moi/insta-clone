66c02bfc732de61d06d81c852cbeed305dced11c
12moi <12moi>
2022-04-03 08:46:25 +0300

    add updates to models.py files

--- insta/admin.py
+++ insta/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile,  Follow
+from .models import Profile, Post, Comments, Follow
 
 admin.site.register(Profile)
-
+admin.site.register(Post)
+admin.site.register(Comments)
 admin.site.register(Follow)
