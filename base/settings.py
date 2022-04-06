bcabf06344ceecf8eb64b89b259bc061abb607b5
12moi <12moi>
2022-04-06 17:36:21 +0300

    add

--- base/settings.py
+++ base/settings.py
@@ -10,13 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import dj_database_url
+
 import django_heroku
 from decouple import config,Csv
 import cloudinary
+
 import cloudinary.uploader
 import cloudinary.api
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
